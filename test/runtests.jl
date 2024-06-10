1df566b95e07b6ef02bd1489c50d24661fd35a62
David Métivier <46794064+dmetivie@users.noreply.github.com>
2024-06-10 23:34:51 +0200

    example test

--- test/runtests.jl
+++ test/runtests.jl
@@ -92,3 +92,32 @@ end
 
     #TODO add test comparing order = 0 to PeriodicHMM (it should be exactly the same)
 end
+
+@testset "PeriodicHMM" begin
+    Random.seed!(2020)
+    K = 3 # Number of Hidden states
+    T = 11 # Period
+    N = 49_586 # Length of observation
+    D = 6 # dimension of observed variables
+    autoregressive_order = 1
+    size_order = 2^autoregressive_order
+    degree_of_P = 1
+    size_degree_of_P = 2*degree_of_P + 1 
+    trans_θ = 4*(rand(K, K - 1, size_degree_of_P) .- 1/2)
+    Bernoulli_θ = 2*(rand(K, D, size_order, size_degree_of_P) .- 1/2)
+    hmm = Trig2HierarchicalPeriodicHMM([1/3, 1/6, 1/2], trans_θ, Bernoulli_θ, T)
+    z_ini = 1
+    y_past = rand(Bool, autoregressive_order, D)
+    n2t = SmoothPeriodicStatsModels.n_to_t(N,T)
+    z, y = rand(hmm, n2t; y_ini=y_past, z_ini=z_ini, seq=true)
+    
+    trans_θ_guess = rand(K, K-1, size_degree_of_P)
+    trans_θ_guess[:,:,1] .= trans_θ[:,:,1]
+    Bernoulli_θ_guess = zeros(K, D, size_order, size_degree_of_P)
+    Bernoulli_θ_guess[:,:,:,1] = Bernoulli_θ[:,:,:,1]
+    hmm_guess = Trig2HierarchicalPeriodicHMM([1/4, 1/4, 1/2], trans_θ_guess, Bernoulli_θ_guess, T)
+
+    @time "FitMLE SHMM (Baum Welch)" hmm_fit, θq_fit, θy_fit, hist, histo_A, histo_B = fit_mle(hmm_guess, trans_θ_guess, Bernoulli_θ_guess, y, y_past, maxiter=10000, robust=true; display=:iter, silence=true, tol=1e-3, θ_iters=true, n2t=n2t);
+    @test θq_fit ≈ trans_θ rtol = 20e-2
+    @test θy_fit ≈ Bernoulli_θ rtol = 20e-2
+end
