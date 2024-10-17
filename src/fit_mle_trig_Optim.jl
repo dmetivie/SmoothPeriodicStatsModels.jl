7634056f85339faf999a99f480fc7deb5ca6fee2
David Métivier <46794064+dmetivie@users.noreply.github.com>
2024-10-17 11:45:10 +0200

    typo

--- src/fit_mle_trig_Optim.jl
+++ src/fit_mle_trig_Optim.jl
@@ -13,7 +13,7 @@ struct OptimMLE{T,V} <: AbstractMLE where {T, V<:AbstractVector}
 end
 #TODO figure out SecondOrder when needed to supress warning (and have better perf?)
 function Distributions.fit_mle(Opt::OptimMLE, y::AbstractArray; solvekwargs...)
-    OptFunc = OptimizationFunction(Opt.ℓ, Optimization.SecondOrder(Optimization.AutoForwardDiff(), Optimization.AutoZygote()))
+    OptFunc = OptimizationFunction(Opt.ℓ, Optimization.AutoForwardDiff())
     prob = OptimizationProblem(OptFunc, Opt.θ₀, y)
 
     return solve(prob, Opt.solver; solvekwargs...)
